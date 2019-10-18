6856ddce031675f27e1d8c33c9d6a339908839a1
Shashika <shashika.lokuarachchi@aurea.com>
2019-10-18 14:11:23 +0530

    main commit

--- SimpleClass.java
+++ SimpleClass.java
@@ -3,7 +3,7 @@ public class SimpleClass {
     private String name = "Donald Duck";
 
     public static void main(String[] args) {
-        System.out.println("Even though I'm simple, it doesn't mean I should be ignored and this is updated now");
+        System.out.println("Hello Even though I'm simple, it doesn't mean I should be ignored and this is updated now");
 
         SimpleClass me = new SimpleClass();
         me.printName();
